0fb7fda64b0d67052c77d37230dd4eff4caa7ade
isha-akella <iakella2@illinois.edu>
2022-10-17 20:58:48 -0500

    Moving keyboard handler

--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -1,6 +1,7 @@
 #include "x86_desc.h"
 #include "handle.h"
 #include "intr_link.h"
+#include "keyboard.h"
 
 #define START_PIC_VEC 0x20 // start of PIC entries
 /* Function that sets the IDT entries */
--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,10 +1,13 @@
 #include "keyboard.h"
 
 /* array that holds the character corresponding to each scancode */
-static char let_num [OPTION_SIZE] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+static char let_num [OPTION_SIZE] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-', '=', '[', ']', ':', '`', ',', '.', '/', ' ', '\\', '\''};
+
 /* scancode info from https://wiki.osdev.org/PS2_Keyboard && https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html */
 /* array that holds the keyboard scancodes for 0-9 and a-z */
-static char scancode_pressed[OPTION_SIZE] = {0x0B, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24, 0x25, 0x26, 0x32, 0x31, 0x18, 0x19, 0x10, 0x13, 0x1F, 0x14, 0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C};
+static char scancode_pressed[OPTION_SIZE] = {0x0B, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24, 0x25, 
+0x26, 0x32, 0x31, 0x18, 0x19, 0x10, 0x13, 0x1F, 0x14, 0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C, 0x0C, 0x0D, 0x1A, 0x1B, 0x27, 0x29, 0x33, 0x34, 0x35, 0x39, 0x2B, 0x28};
 
 /* 
  * keyboard_init
@@ -26,16 +29,16 @@ void keyboard_init() {
  *   RETURN VALUE: void
 */
 void keyboard_link_handler(){
-    int key = inb(0x60);             //port for keyboard
+    int key = inb(0x60);                            //port for keyboard
     int i;
-    char output = 64;               //initial value to check if lowercase or number
+    char output = 64;                               //initial value to check if lowercase or number
     for(i = 0; i < OPTION_SIZE; i++) {
         if(key == scancode_pressed[i]) {
             output = let_num[i];
         }
     }
-    send_eoi(1);                    //send EOI to primary pic 1
-    if(output != 64) putc(output);      //if the value was detected as a valid key print it
+    send_eoi(1);                                    //send EOI to primary pic 1
+    if(output != 64) putc(output);                  //if the value was detected as a valid key print it
     
     else return;
 }
