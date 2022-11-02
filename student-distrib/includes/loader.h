fbeadbfd863beabd90917f82e3287c727d108205
Ravyu Desktop <ravyusk@gmail.com>
2022-11-02 16:22:37 -0500

    added constants for loader

--- student-distrib/includes/loader.h
+++ student-distrib/includes/loader.h
@@ -1,5 +1,8 @@
 #include "types.h"
 
+#define USER_MEMORY_BASE    0x800000
+#define PROGRAM_VMEM_BASE   0x8000000
+static int num_active_procs;
 extern int32_t load_program(void *addr, int fd);                   /* Reads bytes from an executable file into this address */
 extern int32_t setup_user_page(void *addr);                        /* Addr is the bounds of user page we want to define */
 extern int32_t check_executable(int fd);                           /* Checks that file is a valid executable */
